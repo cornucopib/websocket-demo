12bd95c48842fdba64384a067e872801657d007e
Cornucopib <13040839537@163.com>
2022-02-03 11:15:58 +0800

    去掉无用的代码

--- src/main/java/com/cornucopib/websocketdemo/component/WebSocketServer.java
+++ src/main/java/com/cornucopib/websocketdemo/component/WebSocketServer.java
@@ -1,8 +1,6 @@
 package com.cornucopib.websocketdemo.component;
 
-import com.sun.org.slf4j.internal.LoggerFactory;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
